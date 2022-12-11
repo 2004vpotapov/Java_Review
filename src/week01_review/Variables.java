f7213ff9e0691f450eb940a6897a3a4f090547ce
vgp20 <4potapov@gmail.com>
2022-12-11 18:51:30 -0500

    printStatement

---
+++ src/week01_review/Variables.java
@@ -0,0 +1,21 @@
+package week01_review;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        boolean passed = true;
+        System.out.println("passed = " + passed);
+        String name = "Daniel";
+        String n = "2.5 %";
+        double rate = 26.8,
+                j = 32.54;
+        int year = 1956;
+        char dl = 'A';
+        System.out.println("year = " + year);
+        System.out.println("dl = " + dl);
+
+
+        System.out.println("n = " + n);
+    }
+}
