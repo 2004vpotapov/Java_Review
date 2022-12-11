20acd6c0c0ae2a7c8cc86b4a0245012f3296de37
vgp20 <4potapov@gmail.com>
2022-12-11 18:43:59 -0500

    week1

---
+++ src/week01_review/EscapeSequences.java
@@ -0,0 +1,12 @@
+package week01_review;
+
+public class EscapeSequences {
+    public static void main(String[] args) {
+
+        System.out.println("    ^");
+        System.out.println("   / \\");
+        System.out.println("  /   \\");
+        System.out.println("  -----");
+
+    }
+}
