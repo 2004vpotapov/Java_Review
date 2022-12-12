0962b53dc7e593f1a6ae92d011c24888d9514636
vgp20 <4potapov@gmail.com>
2022-12-11 21:25:27 -0500

    printStatement

---
+++ src/week02_review/Concatination.java
@@ -0,0 +1,14 @@
+package week02_review;
+
+public class Concatination {
+    public static void main(String[] args) {
+
+        String name = "James King";
+        int age = 25;
+
+        System.out.println("Hello " +name);
+
+        System.out.println(name+" is "+age+" years old");
+
+    }
+}
